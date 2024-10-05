4160092ad4ca4d0e9282e99bc4f9f0534cae2584
andreibojor <bojor.andrei@gmail.com>
2024-10-05 18:40:09 +0300

    testing a deployment

--- src/novu/workflows/welcome-onboarding-email/workflow.ts
+++ src/novu/workflows/welcome-onboarding-email/workflow.ts
@@ -1,13 +1,12 @@
-import { workflow } from "@novu/framework";
-import { renderEmail } from "../../emails/novu-onboarding-email";
-import { emailControlSchema, payloadSchema } from "./schemas";
+import { renderEmail } from '../../emails/novu-onboarding-email';
+import { emailControlSchema, payloadSchema } from './schemas';
 
 export const welcomeOnboardingEmail = workflow(
-  "welcome-onboarding-email",
+  'welcome-onboarding-email',
   async ({ step, payload }) => {
     await step.email(
-      "send-email",
-      async (controls) => {
+      'send-email',
+      async (controls: any) => {
         return {
           subject: controls.subject,
           body: renderEmail(controls, payload),
@@ -22,3 +21,26 @@ export const welcomeOnboardingEmail = workflow(
     payloadSchema,
   },
 );
+function workflow(
+  arg0: string,
+  arg1: ({ step, payload }: { step: any; payload: any }) => Promise<void>,
+  arg2: {
+    payloadSchema: import('zod').ZodObject<
+      {
+        teamImage: import('zod').ZodDefault<import('zod').ZodString>;
+        userImage: import('zod').ZodDefault<import('zod').ZodString>;
+        arrowImage: import('zod').ZodDefault<import('zod').ZodString>;
+      },
+      'strip',
+      import('zod').ZodTypeAny,
+      { teamImage: string; userImage: string; arrowImage: string },
+      {
+        teamImage?: string | undefined;
+        userImage?: string | undefined;
+        arrowImage?: string | undefined;
+      }
+    >;
+  },
+) {
+  throw new Error('Function not implemented.');
+}
