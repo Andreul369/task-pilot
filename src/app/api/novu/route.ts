df4aaea44dba7b8e34184fd06702dd91abefe1ae
andreibojor <bojor.andrei@gmail.com>
2024-10-05 18:41:09 +0300

    testing deployment

--- src/app/api/novu/route.ts
+++ src/app/api/novu/route.ts
@@ -1,8 +1,13 @@
-import { serve } from '@novu/framework/next';
-
-import { welcomeOnboardingEmail } from '../../novu/workflows';
+import { welcomeOnboardingEmail } from '@/novu/workflows';
 
 // the workflows collection can hold as many workflow definitions as you need
 export const { GET, POST, OPTIONS } = serve({
   workflows: [welcomeOnboardingEmail],
 });
+function serve(arg0: { workflows: void[] }): {
+  GET: any;
+  POST: any;
+  OPTIONS: any;
+} {
+  throw new Error('Function not implemented.');
+}
