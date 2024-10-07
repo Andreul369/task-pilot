4a3b09a1a4c5d57111d794b52e768f82cce1665d
andreibojor <bojor.andrei@gmail.com>
2024-10-07 18:19:27 +0300

    Refactor board layout and remove unused code

--- src/app/(dashboard)/board/layout.tsx
+++ src/app/(dashboard)/board/layout.tsx
@@ -1,22 +1,3 @@
-import Link from 'next/link';
-import { Bell } from 'lucide-react';
-
-import { getUserWorkspaces } from '@/actions/workspaces';
-import { siteConfig } from '@/app/config';
-// import { siteConfig } from '~/app/config';
-// import { SiteFooter } from '~/components/footer';
-// import { MobileDropdown } from '~/components/mobile-nav';
-
-import * as Icons from '@/components/icons/icons';
-import SidebarWorkspace from '@/components/nav/sidebar-workspace';
-import SidebarWorkspaceMobile from '@/components/nav/sidebar-workspace-mobile';
-import { UserNav } from '@/components/nav/user-nav';
-import { Button, buttonVariants, Input } from '@/components/ui';
-import { WorkspaceSwitcher } from '@/components/workspace-switcher';
-import { createClient } from '@/utils/supabase/server';
-
-// import { MainNav } from '../(dashboard)/_components/main-nav';
-
 export default async function BoardLayout({
   children,
 }: Readonly<{
