b07ea32b5e6a031260c329d20da8dd8fe7ee05b5
andreibojor <bojor.andrei@gmail.com>
2024-11-07 16:09:59 +0200

    asd

--- src/components/nav/user-dropdown.tsx
+++ src/components/nav/user-dropdown.tsx
@@ -39,9 +39,9 @@ const UserDropdown = ({
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src={avatarUrl} alt={fullName ?? ''} />
+              {/* <AvatarImage src={avatarUrl} alt={fullName ?? ''} /> */}
               <AvatarFallback>
-                {/* {fullName && getInitials(fullName)} */}
+                {/* {fullName && getInitials(fullName)} */}AA
               </AvatarFallback>
             </Avatar>
           </Button>
@@ -49,9 +49,9 @@ const UserDropdown = ({
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{fullName}</p>
+              {/* <p className="text-sm font-medium leading-none">{fullName}</p> */}
               <p className="text-xs leading-none text-muted-foreground">
-                {email}
+                {/* {email} */}
               </p>
             </div>
           </DropdownMenuLabel>
@@ -85,9 +85,7 @@ const UserDropdown = ({
             <ThemeToggle />
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <SignOut />
-          </DropdownMenuItem>
+          <DropdownMenuItem asChild>{/* <SignOut /> */}</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       <DialogContent className="flex max-w-6xl">
